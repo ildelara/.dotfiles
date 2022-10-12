6862b13f2c1db7febafbd54fa7b2633907303cf4
rowario <r0wario@ya.ru>
2022-10-12 15:21:17 +0300

    updated weather script

--- .local/bin/weather.py
+++ .local/bin/weather.py
@@ -88,8 +88,7 @@ def format_chances(hour):
     return ", ".join(conditions)
 
 
-data['text'] = WEATHER_CODES[weather['current_condition'][0]['weatherCode']] + \
-        " "+weather['current_condition'][0]['FeelsLikeC']+"°"
+data['text'] = weather['current_condition'][0]['FeelsLikeC']+"° "+WEATHER_CODES[weather['current_condition'][0]['weatherCode']]
 
 data['tooltip'] = f"<b>{weather['current_condition'][0]['weatherDesc'][0]['value']} {weather['current_condition'][0]['temp_C']}°</b>\n"
 data['tooltip'] += f"Feels like: {weather['current_condition'][0]['FeelsLikeC']}°\n"
