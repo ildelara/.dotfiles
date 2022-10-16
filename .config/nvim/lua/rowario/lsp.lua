227ecfad78c115f8a51c8de3c3fbe52a4848cf94
rowario <r0wario@ya.ru>
2022-10-16 23:05:44 +0300

    updated packages nvim

--- .config/nvim/lua/rowario/lsp.lua
+++ .config/nvim/lua/rowario/lsp.lua
@@ -35,7 +35,7 @@ cmp.setup({
 
 local function config(_config)
 	return vim.tbl_deep_extend("force", {
-		capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+		capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
 		on_attach = function()
 			Nnoremap("K", ":lua vim.lsp.buf.hover()<CR>")
 			Nnoremap("gd", ":lua vim.lsp.buf.definition()<CR>")
